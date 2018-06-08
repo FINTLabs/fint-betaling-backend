0341a6a8e7afc0407e5c3bb96410e91fc9c835fa
Frode Sjovatsen <frode@fintprosjektet.no>
2018-06-08 13:43:09 +0200

    added springfox

--- src/main/java/no/fint/betaling/Application.java
+++ src/main/java/no/fint/betaling/Application.java
@@ -1,10 +1,12 @@
 package no.fint.betaling;
 
+import com.github.springfox.loader.EnableSpringfox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
+@EnableSpringfox
 @EnableHypermediaSupport(type=EnableHypermediaSupport.HypermediaType.HAL)
 public class Application {
 
