8e0437d524b9a8f82cdf927d8ab2ca38fa02ef6c
Frode Sjovatsen <frode@fintprosjektet.no>
2018-07-03 21:28:31 +0200

    moved mongo config to config file + added banner

--- src/main/java/no/fint/betaling/config/Config.java
+++ src/main/java/no/fint/betaling/config/Config.java
@@ -15,21 +15,11 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class Config {
 
-    private static final String MONGOHOST = "localhost";
-    private static final String MONGOPORT = "27017";
-    private static final String MONGODATABASENAME = "Fint-Betaling";
-
-
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
 
-    @Bean
-    public MongoTemplate getMongoTemplate() {
-        String connectionString = String.format("mongodb://%s:%s/%s", MONGOHOST, MONGOPORT, MONGODATABASENAME);
-        return new MongoTemplate(new MongoClient(new MongoClientURI(connectionString)), MONGODATABASENAME);
-    }
 
     @Autowired
     private ObjectMapper objectMapper;
