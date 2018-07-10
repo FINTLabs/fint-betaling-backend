e3e1a2fc96ba2124ee8bd7dae111aa927333b5c8
noen16 <ulrikro@gmail.com>
2018-07-10 14:58:44 +0200

    added api/me endpoint

---
+++ src/main/java/no/fint/betaling/controller/MeController.java
@@ -0,0 +1,22 @@
+package no.fint.betaling.controller;
+
+import no.fint.betaling.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(value = "/api/test")
+public class MeController {
+
+    @GetMapping
+    public ResponseEntity getMe(){
+        User user = new User();
+        user.setName("Ola Nordmann");
+        user.setOrganisation("Rogaland fylkeskommune");
+        return ResponseEntity.ok(user);
+    }
+}
---
+++ src/main/java/no/fint/betaling/model/User.java
@@ -0,0 +1,9 @@
+package no.fint.betaling.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private String organisation;
+}
