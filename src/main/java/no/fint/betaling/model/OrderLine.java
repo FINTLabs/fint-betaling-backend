3c1fd86890dc75a4bcf7650d5f04925b6b209b50
noen16 <ulrikro@gmail.com>
2018-07-11 08:40:51 +0200

    corrected spelling in orderline model

--- src/main/java/no/fint/betaling/model/OrderLine.java
+++ src/main/java/no/fint/betaling/model/OrderLine.java
@@ -6,6 +6,6 @@ import no.fint.model.resource.administrasjon.okonomi.VarelinjeResource;
 @Data
 public class OrderLine {
     private VarelinjeResource orderLine;
-    private String amount;
+    private Long amount;
     private String description;
 }
