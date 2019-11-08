30b12799accdc7f2e17bb2d28456683a2ca62a59
torgravels <torleif@fintlabs.no>
2019-11-08 11:15:41 +0100

    Update DateRangeController.java

--- src/main/java/no/fint/betaling/controller/DateRangeController.java
+++ src/main/java/no/fint/betaling/controller/DateRangeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(value = "/api/dateRange")
+@RequestMapping(value = "/api/date-range")
 public class DateRangeController {
 
     @Value("${fint.betaling.date-range}")
