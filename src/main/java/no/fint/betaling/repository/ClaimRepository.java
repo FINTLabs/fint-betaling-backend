fa242280b7acd56fba60aef8ead26ef2945fdc4e
torgravels <torleif@fintlabs.no>
2019-11-14 11:04:35 +0100

    Fix

--- src/main/java/no/fint/betaling/repository/ClaimRepository.java
+++ src/main/java/no/fint/betaling/repository/ClaimRepository.java
@@ -2,6 +2,7 @@ package no.fint.betaling.repository;
 
 import no.fint.betaling.model.Claim;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
@@ -15,15 +16,18 @@ public class ClaimRepository {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    @Value("${fint.betaling.org-id}")
+    private String orgId;
+
     public void setClaim(Claim claim) {
-        mongoTemplate.save(claim);
+        mongoTemplate.save(claim, orgId);
     }
 
     public List<Claim> getClaims(Query query) {
-        return mongoTemplate.find(query, Claim.class);
+        return mongoTemplate.find(query, Claim.class, orgId);
     }
 
     public void updateClaim(Query query, Update update) {
-        mongoTemplate.upsert(query, update, Claim.class);
+        mongoTemplate.upsert(query, update, orgId);
     }
 }
