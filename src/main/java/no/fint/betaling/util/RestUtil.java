75eca266d7c760bd24cabc8ea89c6d70887a10cb
Asgeir Nilsen <asgeir@fintlabs.no>
2019-11-21 11:02:35 +0100

    Updated RestUtil and added spec.

--- src/main/java/no/fint/betaling/util/RestUtil.java
+++ src/main/java/no/fint/betaling/util/RestUtil.java
@@ -3,10 +3,6 @@ package no.fint.betaling.util;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.betaling.exception.InvalidResponseException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientResponseException;
@@ -25,32 +21,29 @@ public class RestUtil {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Value("${fint.betaling.org-id}")
-    private String orgId;
+    private final Map<URI, Long> lastUpdatedMap = Collections.synchronizedMap(new HashMap<>());
 
-    private final Map<String, Long> lastUpdatedMap = Collections.synchronizedMap(new HashMap<>());
-
-    public <T> T getUpdates(Class<T> type, URI url) {
-        String key = orgId + "_" + url;
+    public <T> T getUpdates(Class<T> type, URI uri) {
         long lastUpdated = Long.parseLong(
                 get(Map.class, UriComponentsBuilder
-                                .fromUri(url)
-                                .queryParam("/last-updated")
+                                .fromUri(uri)
+                                .pathSegment("last-updated")
                                 .build()
                                 .toUri())
                         .get("lastUpdated")
                         .toString()
         );
-        long since = lastUpdatedMap.getOrDefault(key, -1L) + 1L;
-        log.info("{}: Fetching {} since {}, last updated {} ...", orgId, url, since, lastUpdated);
-        T result = get(type, UriComponentsBuilder.fromUri(url).queryParam("sinceTimeStamp", since).build().toUri());
-        lastUpdatedMap.put(key, lastUpdated);
+        long since = lastUpdatedMap.getOrDefault(uri, -1L) + 1L;
+        log.info("Fetching {} since {}, last updated {} ...", uri, since, lastUpdated);
+        T result = get(type, UriComponentsBuilder.fromUri(uri).queryParam("sinceTimeStamp", since).build().toUri());
+        lastUpdatedMap.put(uri, lastUpdated);
         return result;
     }
 
-    public <T> T get(Class<T> clazz, URI url) {
+    public <T> T get(Class<T> clazz, URI uri) {
         try {
-            return restTemplate.getForObject(url, clazz);
+            log.debug("Get {}", uri);
+            return restTemplate.getForObject(uri, clazz);
         } catch (RestClientResponseException e) {
             throw new InvalidResponseException(e.getResponseBodyAsString(), e);
         }
--- src/test/groovy/no/fint/betaling/util/RestUtilSpec.groovy
+++ src/test/groovy/no/fint/betaling/util/RestUtilSpec.groovy
@@ -2,6 +2,9 @@ package no.fint.betaling.util
 
 import no.fint.betaling.exception.InvalidResponseException
 import no.fint.model.felles.kompleksedatatyper.Identifikator
+import no.fint.model.resource.AbstractCollectionResources
+import no.fint.model.resource.administrasjon.personal.PersonalressursResource
+import no.fint.model.resource.administrasjon.personal.PersonalressursResources
 import no.fint.model.resource.utdanning.elev.ElevResource
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -59,7 +62,42 @@ class RestUtilSpec extends Specification {
         def response = restUtil.post(ElevResource.class, 'http://localhost'.toURI(), new ElevResource(elevnummer: new Identifikator(identifikatorverdi: 'test')))
 
         then:
-        1 * restTemplate.postForEntity('http://localhost'.toURI(),_ , _ as Class<ElevResource>) >> ResponseEntity.ok().build()
+        1 * restTemplate.postForEntity('http://localhost'.toURI(), _, _ as Class<ElevResource>) >> ResponseEntity.ok().build()
         response.statusCode.is2xxSuccessful()
+    }
+
+    def 'Get updates for a resource'() {
+        given:
+        def uri = 'https://play-with-fint.felleskomponent.no/administrasjon/personal/personalressurs'.toURI()
+
+        when:
+        def result = restUtil.getUpdates(PersonalressursResources, uri)
+
+        then:
+        result.totalItems == 1
+        1 * restTemplate.getForObject(
+                'https://play-with-fint.felleskomponent.no/administrasjon/personal/personalressurs/last-updated'.toURI(),
+                _
+        ) >> ['lastUpdated': "12345"]
+        1 * restTemplate.getForObject(
+                'https://play-with-fint.felleskomponent.no/administrasjon/personal/personalressurs?sinceTimeStamp=0'.toURI(),
+                _
+        ) >> new PersonalressursResources(embedded: new AbstractCollectionResources.EmbeddedResources<PersonalressursResource>(entries:
+                [new PersonalressursResource(ansattnummer: new Identifikator(identifikatorverdi: '123456'))]))
+
+        when:
+        def result2 = restUtil.getUpdates(PersonalressursResources, uri)
+
+        then:
+        result2.totalItems == 0
+        1 * restTemplate.getForObject(
+                'https://play-with-fint.felleskomponent.no/administrasjon/personal/personalressurs/last-updated'.toURI(),
+                _
+        ) >> ['lastUpdated': "12345"]
+        1 * restTemplate.getForObject(
+                'https://play-with-fint.felleskomponent.no/administrasjon/personal/personalressurs?sinceTimeStamp=12346'.toURI(),
+                _
+        ) >> new PersonalressursResources(embedded: new AbstractCollectionResources.EmbeddedResources<PersonalressursResource>(entries: []))
+
     }
 }
