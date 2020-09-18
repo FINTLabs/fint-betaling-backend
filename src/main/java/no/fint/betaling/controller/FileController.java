c825644f5867b4a6410c02c9e8587ee68847b61e
OlaStalberg <olastaalberg@gmail.com>
2020-09-18 14:31:58 +0200

    added newest version of betaling model

--- src/main/java/no/fint/betaling/controller/FileController.java
+++ src/main/java/no/fint/betaling/controller/FileController.java
@@ -3,6 +3,7 @@ package no.fint.betaling.controller;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.betaling.service.FileService;
 import no.fint.betaling.util.CustomerFileGroup;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +22,13 @@ public class FileController {
     }
 
     @PostMapping
-    public ResponseEntity<CustomerFileGroup> getCustomersOnFile(@RequestHeader(name = "x-school-org-id") String schoolId, @RequestBody byte[] file) throws IOException {
-        return ResponseEntity.ok(fileService.getCustomersFromFile(schoolId, file));
+    public ResponseEntity getCustomersOnFile(@RequestHeader(name = "x-school-org-id") String schoolId, @RequestBody byte[] file) {
+        ResponseEntity<CustomerFileGroup> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        try {
+            response = ResponseEntity.ok(fileService.getCustomersFromFile(schoolId, file));
+        }catch (Exception ioe){
+            ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body("Supported files are of type: .xls and .xlsx");
+        }
+        return response;
     }
 }
