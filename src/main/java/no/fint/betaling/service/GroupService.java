772cd0d237221fc8bec08b5e515f275cb74ffc55
Trond Magnus Sevre <trond@fintlabs.no>
2021-12-23 08:32:35 +0100

    FB-120 #comment Fails when uploading students from VIS

--- src/main/java/no/fint/betaling/service/GroupService.java
+++ src/main/java/no/fint/betaling/service/GroupService.java
@@ -52,6 +52,7 @@ public class GroupService {
                         e -> e.getSystemId().getIdentifikatorverdi(),
                         e -> e.getElev()
                                 .stream()
+                                .filter(Objects::nonNull)
                                 .map(groupRepository.getStudents()::get)
                                 .map(CustomerFactory::toCustomer)
                                 .distinct()
