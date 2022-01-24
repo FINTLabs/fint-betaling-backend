6f7a34bb7edb230b54752a43fdbef517ab0c6dc6
Trond Magnus Sevre <trond@fintlabs.no>
2022-01-24 11:08:47 +0100

    FB-118 Add pageing + config changes for PWF

--- src/main/java/no/fint/betaling/controller/ClaimController.java
+++ src/main/java/no/fint/betaling/controller/ClaimController.java
@@ -7,6 +7,7 @@ import no.fint.betaling.model.Order;
 import no.fint.betaling.service.ClaimService;
 import no.fint.betaling.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -41,6 +42,11 @@ public class ClaimController {
     @GetMapping
     public List<Claim> getAllClaims() {
         return claimService.getClaims();
+    }
+
+    @GetMapping("paged")
+    public Page<Claim> getClaimsPaged(@RequestParam int page, @RequestParam int size, @RequestParam(defaultValue = "id,desc") String[] sort) {
+        return claimService.getClaimsPaged(page, size, sort);
     }
 
     @GetMapping("/name/{name}")
--- src/main/java/no/fint/betaling/service/ClaimService.java
+++ src/main/java/no/fint/betaling/service/ClaimService.java
@@ -16,6 +16,8 @@ import no.fint.model.resource.okonomi.faktura.FakturagrunnlagResource;
 import org.jooq.lambda.function.Consumer2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.HttpHeaders;
@@ -23,11 +25,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -265,5 +265,9 @@ public class ClaimService {
                 .filter(claim -> claim.getClaimStatus().equals(ClaimStatus.STORED))
                 .peek(claim -> claim.setClaimStatus(ClaimStatus.CANCELLED))
                 .forEach(this::updateClaimStatus);
+    }
+
+    public Page<Claim> getClaimsPaged(int page, int size, String[] sort) {
+        return claimRepository.getClaimsWithPagination(queryService.createQuery(), PageRequest.of(page, size));
     }
 }
