c2755c9ba3b058bbbf80749f5bf75fe5bcd3343f
Trond Magnus Sevre <trond@fintlabs.no>
2022-11-28 15:27:20 +0100

    Change to a single org-id setting - to make thing consistent

--- src/main/java/no/fint/betaling/SecurityConfiguration.java
+++ src/main/java/no/fint/betaling/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package no.fint.betaling;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -7,11 +8,12 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@Slf4j
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
 
-    @Value("${fint.betaling.authorized-org-id:vigo.no}")
-    private String authorizedOrgId;
+    @Value("${fint.betaling.org-id}")
+    private String orgId;
 
     @Value("${fint.betaling.authorized-role:https://role-catalog.vigoiks.no/vigo/elevfakturering/user}")
     private String authorizedRole;
@@ -22,6 +24,8 @@ public class SecurityConfiguration {
     @Bean
     @ConditionalOnProperty(value="fint.betaling.demo", havingValue="true")
     SecurityWebFilterChain springSecurityFilterChainDemo(ServerHttpSecurity http) {
+
+        log.warn("Starting WITHOUT security. SecurityWebFilterChain disabled.");
 
         return http
                 .csrf()
@@ -37,9 +41,8 @@ public class SecurityConfiguration {
         http
                 .authorizeExchange((authorize) -> authorize
                         .pathMatchers("/**")
-                        .hasAuthority("ORGID_" + authorizedOrgId)
+                        .hasAuthority("ORGID_" + orgId)
                         .pathMatchers("/**")
-                        //.hasAuthority("ROLE_" + authorizedRole)
                         .hasAnyAuthority("ROLE_" + authorizedRoleAdmin, "ROLE_" + authorizedRole)
                         .anyExchange()
                         .authenticated())
