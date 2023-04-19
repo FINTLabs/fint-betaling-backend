210b6d9582ed726f30e3bcbc2d764f45185d4425
Trond Magnus Sevre <trond@fintlabs.no>
2023-04-19 18:34:41 +0200

    CT-628 Added ignored test

--- src/test/groovy/no/fint/betaling/controller/UserControllerSpec.groovy
+++ src/test/groovy/no/fint/betaling/controller/UserControllerSpec.groovy
@@ -1,22 +1,71 @@
 package no.fint.betaling.controller
 
-import no.fint.betaling.repository.UserRepository
+import no.fint.betaling.config.ApplicationProperties
+import no.fint.betaling.exception.EmployeeIdException
+import no.fint.betaling.model.User
+import no.fint.betaling.service.UserCacheService
+import org.springframework.http.HttpStatus
+import org.springframework.security.oauth2.jwt.Jwt
+import reactor.test.StepVerifier
 import spock.lang.Ignore
 import spock.lang.Specification
+import reactor.core.publisher.Mono
 
-class UserControllerSpec extends Specification {
+class UserControllerTest extends Specification {
+
+    UserController userController
+    ApplicationProperties applicationProperties = Stub()
+    UserCacheService userCacheService = Mock()
+
+    def setup() {
+        userController = new UserController(applicationProperties, userCacheService)
+    }
 
     @Ignore
-    def "Get me returns user"() {
+    def "getMe returns user information"() {
         given:
-        def controller = new UserController(Mock(UserRepository), applicationProperties)
-        def mockMvc = standaloneSetup(controller)
+        Jwt jwt = Stub(Jwt) {
+            getClaim("employeeId") >> "12345"
+            getClaim("roles") >> ["https://role-catalog.vigoiks.no/vigo/elevfakturering/admin"]
+        }
+        applicationProperties.getDemo() >> false
+        applicationProperties.getDemoUserEmployeeId() >> "12345"
+        User testUser = new User()
+        userCacheService.getUser(_, _) >> Mono.just(testUser)
 
         when:
-        def response = mockMvc.perform(get('/api/me'))
+        def responseMono = userController.getMe(jwt)
 
         then:
-        response.andExpect(status().isOk())
-                .andExpect(jsonPathEquals('$.name', 'Navn Navnesen'))
+        1 * userCacheService.getUser(_, _)
+        StepVerifier.create(responseMono)
+                .assertNext { response ->
+                    assert response.getStatusCode() == HttpStatus.OK
+                }
+                .verifyComplete()
+    }
+
+    @Ignore
+    def "getMe returns BAD_REQUEST when employeeId is empty"() {
+        given:
+        Jwt jwt = Stub(Jwt)
+        applicationProperties.getDemo() >> false
+        applicationProperties.getDemoUserEmployeeId() >> ""
+
+        when:
+        def response = userController.getMe(jwt)
+
+        then:
+        1 * userCacheService.getUser(_, _) >> { throw new EmployeeIdException(HttpStatus.BAD_REQUEST, "Brukerautorisering mangler nødvendig informasjon (employeeId)!") }
+        response.block().getStatusCode() == HttpStatus.BAD_REQUEST
+    }
+
+    def "ping returns ok response"() {
+        when:
+        def response = userController.ping()
+
+        then:
+        response.getStatusCode() == HttpStatus.OK
+        response.getBody() == "Greetings from FINTLabs :)"
     }
 }
