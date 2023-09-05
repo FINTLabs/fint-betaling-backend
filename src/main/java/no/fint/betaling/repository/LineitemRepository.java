f2660b1bc150653caac8980a6ce707a4a98496cd
Trond Magnus Sevre <trond@fintlabs.no>
2023-09-05 10:52:13 +0200

    More logging on updating vare

--- src/main/java/no/fint/betaling/repository/LineitemRepository.java
+++ src/main/java/no/fint/betaling/repository/LineitemRepository.java
@@ -51,30 +51,35 @@ public class LineitemRepository {
     @Scheduled(initialDelay = 1000L, fixedDelayString = "${fint.betaling.refresh-rate:1200000}")
     public void updateLineitems() {
         log.info("Updating vare from {} ...", endpoints.getVare());
-        restUtil.getUpdates(VareResources.class, endpoints.getVare())
-                .block()
-                .getContent()
-                .forEach(v -> {
-                    Lineitem lineitem = new Lineitem();
-                    //lineitem.setItemCode(v.getSystemId().getIdentifikatorverdi());
-                    lineitem.setItemCode(v.getKode());
-                    lineitem.setItemPrice(v.getPris());
-                    lineitem.setDescription(v.getNavn());
-                    v.getMerverdiavgift()
-                            .stream()
-                            .map(Link::getHref)
-                            //.map(href -> StringUtils.substringAfterLast(href, "/"))
-                            .map(taxcodeRepository::getTaxcodeByUri)
-                            .filter(Objects::nonNull)
-                            .map(Taxcode::getRate)
-                            .forEach(lineitem::setTaxrate);
-                    v.getSelfLinks()
-                            .stream()
-                            .map(Link::getHref)
-                            .forEach(lineitem::setUri);
-                    lineitems.put(lineitem.getUri(), lineitem);
-                });
-        log.info("Update completed, {} varer.", lineitems.size());
-    }
+        try {
+            restUtil.getUpdates(VareResources.class, endpoints.getVare())
+                    .block()
+                    .getContent()
+                    .forEach(v -> {
+                        Lineitem lineitem = new Lineitem();
+                        //lineitem.setItemCode(v.getSystemId().getIdentifikatorverdi());
+                        lineitem.setItemCode(v.getKode());
+                        lineitem.setItemPrice(v.getPris());
+                        lineitem.setDescription(v.getNavn());
+                        v.getMerverdiavgift()
+                                .stream()
+                                .map(Link::getHref)
+                                //.map(href -> StringUtils.substringAfterLast(href, "/"))
+                                .map(taxcodeRepository::getTaxcodeByUri)
+                                .filter(Objects::nonNull)
+                                .map(Taxcode::getRate)
+                                .forEach(lineitem::setTaxrate);
+                        v.getSelfLinks()
+                                .stream()
+                                .map(Link::getHref)
+                                .forEach(lineitem::setUri);
+                        lineitems.put(lineitem.getUri(), lineitem);
+                    });
 
+            log.info("Update completed, {} varer.", lineitems.size());
+
+        } catch (Exception exception) {
+            log.error("Updating vare failed: " + exception.getMessage(), exception);
+        }
+    }
 }
