45767ce9b38afe0063a8b36b8d6de8e1fe2439db
Trond Magnus Sevre <trond@fintlabs.no>
2024-01-04 13:41:52 +0100

    Inital code to test jpa repository

---
+++ src/test/groovy/no/fint/betaling/repository/ClaimJpaRepositorySpec.groovy
@@ -0,0 +1,87 @@
+package no.fint.betaling.repository
+
+import no.fint.betaling.model.ClaimStatus
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import spock.lang.Ignore
+import spock.lang.Specification
+
+import java.time.LocalDate
+
+@Ignore("Not implemented yet")
+@DataJpaTest
+class ClaimJpaRepositorySpec extends Specification {
+
+    @Autowired
+    ClaimJpaRepository claimJpaRepository
+
+    def setupSpec() {
+        // Initialisering av testdata kan gjøres her, hvis nødvendig
+    }
+
+    def "should find highest order number"() {
+        when:
+        def highestOrderNumber = claimJpaRepository.findHighestOrderNumber()
+
+        then:
+        highestOrderNumber.isPresent()
+    }
+
+    def "should find claims by status"() {
+        given:
+        def status = ClaimStatus.ACCEPTED
+
+        when:
+        def claims = claimJpaRepository.findByClaimStatusIn(status)
+
+        then:
+        claims.size() > 0
+    }
+
+    def "should find claims by customer name"() {
+        given:
+        def customerName = "John Doe"
+
+        when:
+        def claims = claimJpaRepository.findByCustomerName(customerName)
+
+        then:
+        claims.size() > 0
+    }
+
+    def "should count claims by status"() {
+        given:
+        def status = ClaimStatus.SENT
+
+        when:
+        def count = claimJpaRepository.countByStatus(status)
+
+        then:
+        count >= 0
+    }
+
+    def "should count claims by status and days"() {
+        given:
+        def status = ClaimStatus.REJECTED
+        def days = 30
+
+        when:
+        def count = claimJpaRepository.countByStatusAndDays(days, status)
+
+        then:
+        count >= 0
+    }
+
+    def "should find claims by date, school, and status"() {
+        given:
+        def date = LocalDate.now()
+        def school = "SchoolName"
+        def status = [ClaimStatus.ACCEPTED, ClaimStatus.SENT]
+
+        when:
+        def claims = claimJpaRepository.getByDateAndSchoolAndStatus(date, school, status)
+
+        then:
+        claims.size() >= 0
+    }
+}
