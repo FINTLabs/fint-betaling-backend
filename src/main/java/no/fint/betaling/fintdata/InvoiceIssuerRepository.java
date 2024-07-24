f1e0efb6ea46de981ec151f62a7fcc358f5dfa19
Trond Magnus Sevre <trond@fintlabs.no>
2024-07-24 13:18:25 +0200

    Fix InvoiceIssuerRepository

--- src/main/java/no/fint/betaling/fintdata/InvoiceIssuerRepository.java
+++ src/main/java/no/fint/betaling/fintdata/InvoiceIssuerRepository.java
@@ -3,6 +3,7 @@ package no.fint.betaling.fintdata;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.betaling.common.config.Endpoints;
 import no.fint.betaling.common.util.RestUtil;
+import no.fint.betaling.model.Lineitem;
 import no.fint.betaling.model.Principal;
 import no.fint.betaling.organisation.OrganisationRepository;
 import no.fint.model.resource.Link;
@@ -10,21 +11,19 @@ import no.fint.model.resource.okonomi.faktura.FakturautstederResource;
 import no.fint.model.resource.okonomi.faktura.FakturautstederResources;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Repository
-public class InvoiceIssuerRepository extends FintResourceRepository<FakturautstederResource, FakturautstederResources>{
+public class InvoiceIssuerRepository extends FintResourceRepository<FakturautstederResource, FakturautstederResources> {
 
     private final LineItemRepository lineitemRepository;
     private final OrganisationRepository organisationRepository;
 
-    private final ConcurrentMap<String, Principal> invoiceIssuers = new ConcurrentSkipListMap<>();
+    private List<Principal> invoiceIssuers = new ArrayList<>();
 
     public InvoiceIssuerRepository(RestUtil restUtil, Endpoints endpoints, LineItemRepository lineitemRepository, OrganisationRepository organisationRepository) {
         super(restUtil, endpoints.getInvoiceIssuer(), FakturautstederResources.class);
@@ -34,36 +33,40 @@ public class InvoiceIssuerRepository extends FintResourceRepository<Fakturautste
 
     public Collection<Principal> getInvoiceIssuers() {
         if (invoiceIssuers.isEmpty()) {
-            updateInvoiceIssuers();
+            update();
         }
-        return Collections.unmodifiableCollection(invoiceIssuers.values());
+        return Collections.unmodifiableCollection(invoiceIssuers);
     }
 
-    public void updateInvoiceIssuers() {
-        log.info("Updating invoice issuer from {} ...", endpoint);
-        restUtil.getUpdates(FakturautstederResources.class, endpoint)
-                .block()
-                .getContent()
-                .forEach(fakturautsteder -> {
-                    Principal principal = new Principal();
-                    if (isEmpty()) return;
-                    principal.setOrganisation(organisationRepository.getOrganisationByHref(fakturautsteder.getOrganisasjonselement().get(0).getHref()));
-                    principal.setCode(fakturautsteder.getSystemId().getIdentifikatorverdi());
-                    principal.setDescription(fakturautsteder.getNavn());
-                    principal.setLineitems(fakturautsteder.getVare()
-                            .stream()
-                            .map(Link::getHref)
-                            .map(lineitemRepository::getLineItemByUri)
-                            .filter(Objects::nonNull)
-                            .collect(Collectors.toSet()));
-                    fakturautsteder.getSelfLinks()
-                            .stream()
-                            .map(Link::getHref)
-                            .forEach(principal::setUri);
-                    invoiceIssuers.put(principal.getUri(), principal);
-                });
-        log.info("Update completed, {} invoice issuers.", invoiceIssuers.size());
+    @Override
+    protected void onResourcesUpdated() {
+        List<Principal> updatedList = resources
+                .values()
+                .stream()
+                .map(this::createPrincipal)
+                .collect(Collectors.toList());
+
+        if (!updatedList.isEmpty()) {
+            invoiceIssuers = updatedList;
+        }
     }
 
+    private Principal createPrincipal(FakturautstederResource fakturautsteder) {
+        Principal principal = new Principal();
+        principal.setOrganisation(organisationRepository.getOrganisationByHref(fakturautsteder.getOrganisasjonselement().get(0).getHref()));
+        principal.setCode(fakturautsteder.getSystemId().getIdentifikatorverdi());
+        principal.setDescription(fakturautsteder.getNavn());
+        principal.setLineitems(fakturautsteder.getVare()
+                .stream()
+                .map(Link::getHref)
+                .map(lineitemRepository::getLineItemByUri)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet()));
+        fakturautsteder.getSelfLinks()
+                .stream()
+                .map(Link::getHref)
+                .forEach(principal::setUri);
 
+        return principal;
+    }
 }
