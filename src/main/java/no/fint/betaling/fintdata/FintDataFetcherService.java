59b6c3b10b6bde109da7e031cf2b6461a872920b
Trond Magnus Sevre <trond@fintlabs.no>
2024-07-25 14:14:38 +0200

    Reactive scheduling data fetching

--- src/main/java/no/fint/betaling/fintdata/FintDataFetcherService.java
+++ src/main/java/no/fint/betaling/fintdata/FintDataFetcherService.java
@@ -1,50 +1,54 @@
 package no.fint.betaling.fintdata;
 
 import lombok.extern.slf4j.Slf4j;
-import no.fint.model.resource.okonomi.faktura.FakturautstederResources;
-import no.fint.model.resource.okonomi.kodeverk.VareResources;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import reactor.util.retry.Retry;
-
-import java.time.Duration;
 
 @Slf4j
 @Service
 public class FintDataFetcherService {
 
-    @Scheduled(initialDelay = 1000L, fixedDelayString = "${fint.betaling.refresh-rate:3600000}")
-    public void updateAll() {
-        // TODO: Implement this method
-//        int basisGroupsUpdated = basisGroups.update();
-//        int contactTeachersUpdated = contactTeacherGroups.update();
-//        int studentRelationsUpdated = studentRelations.update();
-//        int studentsUpdated = students.update();
-//        log.info("Updates completed, basisgruppe ({}), undervisningsgruppe ({}), elevforhold ({}), elev ({})", basisGroupsUpdated, contactTeachersUpdated, studentRelationsUpdated, studentsUpdated);
+    private final BasisGroupRepository basisGroupRepository;
+    private final ContactTeacherGroupRepository contactTeacherGroupRepository;
+    private final StudentRepository studentRepository;
+    private final StudentRelationRepository studentRelationRepository;
+    private final SchoolRepository schoolRepository;
+    private final SchoolResourceRepository schoolResourceRepository;
+    private final TaxCodeRepository taxCodeRepository;
+    private final InvoiceIssuerRepository invoiceIssuerRepository;
+    private final LineItemRepository lineItemRepository;
 
-        // fra schools:
-//        log.info("Updates completed, schools ({}), schoolresource ({})", schoolsUpdated, schoolResourceUpdated);
-
-
-        //@Scheduled(initialDelay = 1000L, fixedDelayString = "${fint.betaling.refresh-rate:1200000}")
-        //public void updateTaxcodes() {
-
-        //@Scheduled(initialDelay = 1000L, fixedDelayString = "${fint.betaling.refresh-rate:1200000}")
-        //public void updateLineitems() {
-
-        // I FintResourceRepository må vi se på retry:
-//        restUtil.getUpdates(VareResources.class, endpoints.getVare())
-//                .retryWhen(
-//                        Retry.backoff(5, Duration.ofSeconds(10))
-//                                .maxBackoff(Duration.ofSeconds(60)))
-
-
-//        @Scheduled(initialDelay = 1000L, fixedDelayString = "${fint.betaling.refresh-rate:1200000}")
-//        public void updateInvoiceIssuers() {
-//            log.info("Updating invoice issuer from {} ...", endpoints.getInvoiceIssuer());
-//            restUtil.getUpdates(FakturautstederResources.class, endpoints.getInvoiceIssuer())
-
-
+    public FintDataFetcherService(BasisGroupRepository basisGroupRepository,
+                                  ContactTeacherGroupRepository contactTeacherGroupRepository,
+                                  StudentRepository studentRepository,
+                                  StudentRelationRepository studentRelationRepository,
+                                  SchoolRepository schoolRepository,
+                                  SchoolResourceRepository schoolResourceRepository,
+                                  TaxCodeRepository taxCodeRepository,
+                                  InvoiceIssuerRepository invoiceIssuerRepository,
+                                  LineItemRepository lineItemRepository) {
+        this.basisGroupRepository = basisGroupRepository;
+        this.contactTeacherGroupRepository = contactTeacherGroupRepository;
+        this.studentRepository = studentRepository;
+        this.studentRelationRepository = studentRelationRepository;
+        this.schoolRepository = schoolRepository;
+        this.schoolResourceRepository = schoolResourceRepository;
+        this.taxCodeRepository = taxCodeRepository;
+        this.invoiceIssuerRepository = invoiceIssuerRepository;
+        this.lineItemRepository = lineItemRepository;
     }
 
+    @Scheduled(initialDelay = 1000L, fixedDelayString = "${fint.betaling.refresh-rate:3600000}")
+    public void updateAll() {
+        int basisGroupsUpdated = basisGroupRepository.update();
+        int contactTeacherGroupsUpdated = contactTeacherGroupRepository.update();
+        int studentRelationsUpdated = studentRelationRepository.update();
+        int studentsUpdated = studentRepository.update();
+        int schoolsUpdated = schoolRepository.update();
+        int schoolResourcesUpdated = schoolResourceRepository.update();
+        int taxCodesUpdated = taxCodeRepository.update();
+        int lineItemsUpdated = lineItemRepository.update();
+        int invoiceIssuersUpdated = invoiceIssuerRepository.update();
+        log.info("Oppdateringer fullført: Basisgrupper ({}), Kontaktlærergrupper ({}), Elevforhold ({}), Elever ({}), Skoler ({}), Skoleresurser ({}), Skattekoder ({}), Fakturautstedere ({}), Varelinjer ({})", basisGroupsUpdated, contactTeacherGroupsUpdated, studentRelationsUpdated, studentsUpdated, schoolsUpdated, schoolResourcesUpdated, taxCodesUpdated, invoiceIssuersUpdated, lineItemsUpdated);
+    }
 }
